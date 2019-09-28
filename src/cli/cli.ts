1a996851111009435757a06e575fb96ce308dcf8
claukers <claukers@melia>
2019-09-28 06:35:14 -0300

    db cmds moved to miqro-sequelize

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -7,10 +7,6 @@ const logger = console;
 const cmds: ISimpleMap<{ module: string; description: string }> = {
   start: { module: "./start", description: "starts a microservice" },
   watch: { module: "./watch", description: "starts a microservice in watch mode on the service dir" },
-  makemigrations: { module: "./makemigrations", description: "seeks changes in your models and creates migrations" },
-  migrate: { module: "./migrate", description: "runs the migrations" },
-  automigrate: { module: "./automigrate", description: "runs makemigrations and migrate together" },
-  seed: { module: "./seed", description: "seeds your db" },
   init: { module: "./init", description: "inits your config folder (MIQRO_DIRNAME)" },
   reset: { module: "./reset", description: "delete custom made config files." },
   version: { module: "./version", description: "prints miqro version." }
