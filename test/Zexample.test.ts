f49e683576121c5979756c11329de83efdc33027
claukers <claukers@amelia>
2020-07-26 08:33:00 -0400

    fix axios import

--- test/Zexample.test.ts
+++ test/Zexample.test.ts
@@ -1,5 +1,5 @@
 import {describe, it} from 'mocha';
-import * as axios from "axios";
+import axios from "axios";
 import * as chai from 'chai';
 import * as path from 'path';
 // import * as rewiremock from 'rewiremock';
@@ -36,12 +36,11 @@ describe('Zexample start', function () {
         mode: "simple"
       });
       await micro.start();
-
-      const result = await axios.default.request({url: "http://localhost:8080/hello", method: "get"});
+      const result = await axios.request({url: "http://localhost:8080/hello", method: "get"});
       chai.expect(result.status).to.be.equals(200);
       chai.expect(result.data.result).to.be.equals("world");
-      await axios.default.request({url: "http://localhost:8080/hello", method: "get"});
-      await axios.default.request({url: "http://localhost:8080/hello", method: "get"});
+      await axios.request({url: "http://localhost:8080/hello", method: "get"});
+      await axios.request({url: "http://localhost:8080/hello", method: "get"});
       await micro.stop();
       delete process.env.MIQRO_DIRNAME;
     };
