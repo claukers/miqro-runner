c9a4c484462c01cd834657723da55b41cd4bca58
claukers <claukers@amelia>
2020-08-20 17:00:02 -0400

    MiqroAPI fix bug where stop in simple start doesnt work

--- src/miqroapi.ts
+++ src/miqroapi.ts
@@ -3,10 +3,6 @@ import {Miqro} from "./miqro";
 import {runAPI, setupInstance} from "./loader";
 
 export class MiqroAPI extends Miqro {
-  protected async simpleStop(): Promise<void> {
-    // nothing to do here
-  }
-
   protected async simpleStart(): Promise<void> {
     this.simpleInstance = setupInstance(this.config.name);
     this.instanceApp = await runAPI(this.simpleInstance.logger, this.config.service);
